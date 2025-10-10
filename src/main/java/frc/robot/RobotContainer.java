062b63c0360a1c7acf29669c2b6cf7b1292b9978
bodenaarhus <boden7607@gmail.com>
2025-10-09 20:44:39 -0500

    added real for elevator to robotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -134,28 +134,31 @@ public class RobotContainer {
                 
 
        
-        elevator = new Elevator(
-          new ElevatorIOSim(
-            4,
-            new ElevatorSim(
-              LinearSystemId.createElevatorSystem(
-                DCMotor.getKrakenX60Foc(2), 
-                Pounds.of(45).in(Kilograms),
-                Inches.of(Elevator.SPOOL_RADIUS).in(Meters), 
-                Elevator.REDUCTION
-              ), 
-              DCMotor.getKrakenX60Foc(2), 
-              Inches.of(0).in(Meters),
-              Inches.of(32).in(Meters),
-              true, 
-              Inches.of(0).in(Meters)
-            )
-          )
-        );
-
+        //elevator = new Elevator(
+          //new ElevatorIOSim(
+          // 4,
+          //  new ElevatorSim(
+          //    LinearSystemId.createElevatorSystem(
+          //      DCMotor.getKrakenX60Foc(2), 
+          //      Pounds.of(45).in(Kilograms),
+          //      Inches.of(Elevator.SPOOL_RADIUS).in(Meters), 
+          //      Elevator.REDUCTION
+          //    ), 
+          //    DCMotor.getKrakenX60Foc(2), 
+          //    Inches.of(0).in(Meters),
+          //    Inches.of(32).in(Meters),
+          //    true, 
+          //   Inches.of(0).in(Meters)
+          //  )
+         // )
+        //);
+        
         //TODO: uncomment simulator when 3D sim is implemented
         //cee = new CoralEndEffector(new CoralEndEffectorIOSim(121));
         cee = new CoralEndEffector(new CoralEndEffectorIOTalonFX(rioCanBuilder.id(9).build(), rioCanBuilder.id(21).build()));
+        elevator = new Elevator(new ElevatorIOTalonFX(rioCanBuilder.id(13).build(),rioCanBuilder.id(14).build()));
+
+      
 
         
         SmartDashboard.putData(drive);
