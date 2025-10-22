ccd7b101be04398d651e212d71a26d2882c3b1d1
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2025-10-21 20:40:56 -0500

    Added IntakeCommand
    Added an Intake command.
    Added the intake command in auto command manager.
    When you don't hold the d-pad it will go back to stow.
    Added the score 1 straight auto.

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,7 +1,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.coralendeffector.CoralEndEffector;
 
 public class IntakeCommand extends Command {
-    
+    private CoralEndEffector cee;
+
+    public IntakeCommand(CoralEndEffector cee){
+        this.cee = cee;
+    }
+
+    @Override
+    public void initialize(){
+        cee.getNewSetVoltsCommand(2);
+    }    
+
+    @Override
+    public boolean isFinished(){
+        return cee.getNewHasCoralSupplier().getAsBoolean();
+    }
 }
