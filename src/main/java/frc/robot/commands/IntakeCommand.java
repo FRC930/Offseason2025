38568de3ffa8463ce4586dba19efdcd61caa47df
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2025-10-30 20:37:03 -0500

    Fixed the Commands
    Fixed the commands for intake and scorel1 to actually use the cee.

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.coralendeffector.CoralEndEffector;
 
@@ -12,7 +14,7 @@ public class IntakeCommand extends Command {
 
     @Override
     public void initialize(){
-        cee.getNewSetVoltsCommand(2);
+        cee.setTarget(Volts.of(-0.5));
     }    
 
     @Override
